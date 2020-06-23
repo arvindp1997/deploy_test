536f28cd45acdb1b40e023037c47e9e79e7878e4
arvind <arvindputtu@gmail.com>
2020-06-23 21:36:50 +0530

    my initial commit

---
+++ client/build/precache-manifest.eeb494219004fdb086c45adbc50f46ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aafa2f45b5691d0bd960f3dd73441694",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f176fcd1933b1c307fab",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "88a4cc64d87e2799a052",
+    "url": "/static/css/main.525e767b.chunk.css"
+  },
+  {
+    "revision": "f176fcd1933b1c307fab",
+    "url": "/static/js/2.eae6d9f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.eae6d9f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88a4cc64d87e2799a052",
+    "url": "/static/js/main.c2e5c01b.chunk.js"
+  },
+  {
+    "revision": "770c12391a509994f8e7",
+    "url": "/static/js/runtime-main.c252944d.js"
+  }
+]);
---
+++ client/src/components/institute-list.component.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+
+
+
+
+const Institute = props =>(
+    <tr>
+        <td>{props.institute.institute_name}</td>
+        <td>{props.institute.phone_number}</td>
+        <td>{props.institute.email}</td>
+        <td>{props.institute.address}</td>
+        <td>{props.institute.number_of_students}</td>
+        
+        <td>
+            <Link to={"/edit/" + props.institute._id}>Edit</Link>
+        </td>
+    </tr>
+)
+
+
+
+
+export default class InstitutesList extends Component{
+
+    constructor(props) {
+
+        super(props);
+        this.state = {institutes: []};
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:4000/institute/')
+        .then(response => {
+            this.setState({institutes:response.data});
+            
+        })
+        .catch(function (error){
+            console.log(error);
+        })
+    }
+
+    componentDidUpdate() {
+        axios.get('http://localhost:4000/institute/')
+        .then(response => {
+            this.setState({institutes:response.data});
+            
+        })
+        .catch(function (error){
+            console.log(error);
+        })
+    }
+
+
+
+    instituteList() {
+
+        return this.state.institutes.map(function(currentInstitute,i){
+
+           return <Institute institute={currentInstitute} key={i}/>;
+
+
+        });
+    }
+    
+    
+    render(){
+
+      
+
+    return (
+          
+        
+      
+         <div >
+                <h3>Institutes List</h3>
+
+             
+                <table id="example" class="table table-bordered table-dark table-hover" style={{marginTop:20}} cellSpacing="0" width="100%" >
+                    <thead>
+                        <tr>
+
+                            <th>Institute Name</th>
+                            <th>Phone Number</th>
+                            <th>Email</th>
+                            <th>Address</th>
+                            <th>Number Of Students</th>
+                            <th>Actions</th>
+
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {this.instituteList()}
+                    </tbody>
+
+                   
+                    
+
+                </table>
+   
+
+            </div>
+            
+
+        )
+    }
+    
+
+}
